af8bc9220ecf19522a3bf389223473d72cc69c7e
mathias1020 <mathias1026@gmail.com>
2022-03-24 13:48:01 -0400

    Add files via upload
    Part 1 commit

--- Github_Upload/loan_analyzer.py
+++ Github_Upload/loan_analyzer.py
@@ -19,15 +19,24 @@ loan_costs = [500, 600, 200, 1000, 450]
 # Print the number of loans from the list
 # YOUR CODE HERE!
 
+total_number_of_loans = len(loan_costs)
+print(total_number_of_loans)
+
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
 # YOUR CODE HERE!
 
+sum_of_loans = sum(loan_costs)
+print(sum_of_loans)
+
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
 # YOUR CODE HERE!
+
+average_loan_amount = sum_of_loans / total_number_of_loans
+print(average_loan_amount)
 
 """Part 2: Analyze Loan Data.
 
@@ -107,7 +116,7 @@ new_loan = {
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
+# print(f"The present value of the loan is: {present_value}")
 
 
 """Part 4: Conditionally filter lists of loans.
